937eb29eb18c0e892a772d1a7d7dedc7af394277
Jannis Mattheis <contact@jmattheis.de>
2022-04-10 12:55:59 +0200

    Add lua lsp

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -530,6 +530,22 @@ require'lspconfig'.omnisharp.setup {
     },
     cmd = {omnisharp_bin, "--languageserver", "--hostPID", tostring(pid)}
 }
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+require'lspconfig'.sumneko_lua.setup {
+    on_attach = on_attach,
+    caps = caps,
+    settings = {
+        Lua = {
+            runtime = {version = 'LuaJIT', path = runtime_path},
+            diagnostics = {globals = {'vim'}},
+            workspace = {library = vim.api.nvim_get_runtime_file("", true)},
+            telemetry = {enable = false}
+        }
+    }
+}
 require'rust-tools'.setup({
     server = {on_attach = on_attach, capabilities = caps}
 })
