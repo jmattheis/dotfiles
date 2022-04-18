06b705b170a40b3f38632f41066bcc7421309ea2
Jannis Mattheis <contact@jmattheis.de>
2022-04-18 19:23:59 +0200

    Update nvim tree setting

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -387,15 +387,11 @@ cmp.setup({
         expand = function(args) require('luasnip').lsp_expand(args.body) end
     },
     mapping = {
+        ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i'}),
+        ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item(), {'i'}),
         ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
         ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
         ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
-        ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ['<C-e>'] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close()
-        }),
-        -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ['<CR>'] = cmp.mapping.confirm({select = true})
     },
     sources = cmp.config.sources({{name = 'nvim_lsp'}, {name = 'path'}})
