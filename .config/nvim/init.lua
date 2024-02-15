e4165764662da2163024d58032f0d2bf6dcd61b9
Jannis Mattheis <contact@jmattheis.de>
2024-02-15 13:59:30 +0100

    fix: restore gitcommit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -610,11 +610,13 @@ autocmd('BufRead', {
             buffer = opts.buf,
             callback = function()
                 local ft = vim.bo[opts.buf].filetype
-                local last_known_line =
-                    vim.api.nvim_buf_get_mark(opts.buf, '"')[1]
-                if not (ft:match('commit') and ft:match('rebase')) and
-                    last_known_line > 1 and last_known_line <=
-                    vim.api.nvim_buf_line_count(opts.buf) then
+
+                if ft:match('gitcommit') or ft:match('gitrebase') then
+                    return
+                end
+
+                local last_known_line = vim.api.nvim_buf_get_mark(opts.buf, '"')[1]
+                if last_known_line > 1 and last_known_line <= vim.api.nvim_buf_line_count(opts.buf) then
                     vim.api.nvim_feedkeys([[g`"]], 'nx', false)
                 end
             end
