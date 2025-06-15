0c64a0ffbee06a3f54a608dc363c48f1c24198ca
Jannis Mattheis <contact@jmattheis.de>
2025-06-15 19:14:15 +0200

    fix: avante

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -128,6 +128,38 @@ local plugins = {
 			})
 		end,
 	},
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		version = false, -- Never set this value to "*"! Never!
+		opts = {
+			provider = "openai",
+			openai = {
+				endpoint = "https://openrouter.ai/api/v1",
+				model = "anthropic/claude-3.7-sonnet",
+				api_key_name = "OPENROUTER_API_KEY_AVANTE",
+				temperature = 0.6,
+				max_tokens = 8000,
+			},
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
 	{ -- show spaces / tabs everywhere
 		"lukas-reineke/indent-blankline.nvim",
 		config = function()
